406100ff823872160901bd5eb5716f3762507014
Chris Langford <demilich1988@gmail.com>
2017-05-08 11:22:54 -0400

    Add project files.

---
+++ Vectors/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create two instances to the vector class
+            Vector v1 = new Vector(1, 2,3);
+            Vector v2 = new Vector(3, 3,3);
+
+           
+            //Output the addition and subtraction of the two instances of the vector class
+            Console.WriteLine(v1+v2);
+            Console.WriteLine(v1-v2);
+
+            //Change the direction of a vector
+            Vector v3 = new Vector(2,0,-4);
+            Console.WriteLine(-v3);
+
+            //Multiply a vector
+            Vector v4 = new Vector(1,2,3);
+            Console.WriteLine(v4 * 4);
+
+            //Divide a vector
+            Vector v5 = new Vector(2,4,6);
+            Console.WriteLine(v5/2);
+        }
+    }
+}
---
+++ Vectors/Vector.cs
@@ -0,0 +1,112 @@
+﻿namespace Vectors
+{
+     class Vector
+    {
+       
+
+        //Get and Set for the vector variables
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        
+
+        /// <summary>
+        /// Overloaded operator for addition
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(
+               v1.X + v2.X,
+               v1.Y + v2.Y,
+               v1.Z + v2.Z);
+        }
+
+        /// <summary>
+        /// Overload Operator subtraction
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(
+                v1.X - v2.X,
+                v1.Y - v2.Y,
+                v1.Z - v2.Z
+                );
+        }
+
+        /// <summary>
+        /// Overloaded operator to change direction of vector
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector operator -(Vector v)
+        {
+            return new Vector(-v.X, -v.Y, -v.Z);
+        }
+
+        /// <summary>
+        /// Overloaded Method for operator with scalar
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="scalar"></param>
+        /// <returns></returns>
+        public static Vector operator +(Vector v , double scalar)
+        {
+            return new Vector(v.X+scalar, v.Y+scalar, v.Z+scalar);
+        }
+
+        /// <summary>
+        /// Overload Method for * operator
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="scalar"></param>
+        /// <returns></returns>
+        public static Vector operator *(Vector v, double scalar)
+        {
+            return new Vector(
+                v.X * scalar,
+                v.Y * scalar,
+                v.Z * scalar
+                );
+        }
+
+        /// <summary>
+        /// Overload Method for / operator
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="scalar"></param>
+        /// <returns></returns>
+        public static Vector operator /(Vector v, double scalar)
+        {
+            return new Vector(
+                v.X / scalar,
+                v.Y / scalar,
+                v.Z / scalar
+                );
+        }
+
+        /// <summary>
+        /// Overridden to make it easy to print out vectors.
+        /// This return the vector in the format <x, y, z>
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "<" + X + ", " + Y + ", " + Z + ">";
+        }
+    }
+}
